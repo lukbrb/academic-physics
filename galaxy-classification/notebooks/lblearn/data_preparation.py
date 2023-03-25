38b4b1302f3aabdcfadeb7e66bb62eda59dcc148
Lucas <lucasbarbr@outlook.com>
2023-03-25 17:50:34 +0100

    ajout classe dataset

--- galaxy-classification/notebooks/lblearn/data_preparation.py
+++ galaxy-classification/notebooks/lblearn/data_preparation.py
@@ -3,9 +3,57 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from sklearn.utils._bunch import Bunch
 import os
 
+# From sklearn.utils._bunch import Bunch
+class DataSet(dict):
+    """Container object exposing keys as attributes.
+
+    Bunch objects are sometimes used as an output for functions and methods.
+    They extend dictionaries by enabling values to be accessed by key,
+    `bunch["value_key"]`, or by an attribute, `bunch.value_key`.
+
+    Examples
+    --------
+    >>> from sklearn.utils import Bunch
+    >>> b = Bunch(a=1, b=2)
+    >>> b['b']
+    2
+    >>> b.b
+    2
+    >>> b.a = 3
+    >>> b['a']
+    3
+    >>> b.c = 6
+    >>> b['c']
+    6
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __dir__(self):
+        return self.keys()
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(key)
+
+    def __setstate__(self, state):
+        # Bunch pickles generated with scikit-learn 0.16.* have an non
+        # empty __dict__. This causes a surprising behaviour when
+        # loading these pickles scikit-learn 0.17: reading bunch.key
+        # uses __dict__ but assigning to bunch.key use __setattr__ and
+        # only changes bunch['key']. More details can be found at:
+        # https://github.com/scikit-learn/scikit-learn/issues/6196.
+        # Overriding __setstate__ to be a noop has the effect of
+        # ignoring the pickled __dict__
+        pass
 
 
 GALAXY_FILENAME = "data/GalaxyZoo1_DR_table2.csv"
@@ -43,7 +91,7 @@ def load_galaxies(*, return_X_y=False, as_frame=False):
     target = target_matrix.max(axis=1) - 1
 
     frame = None
-    return Bunch(
+    return DataSet(
         data=data,
         target=target,
         frame=frame,
